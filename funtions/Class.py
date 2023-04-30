19249da41e86e98698116b3d753fa9a74665652d
Arielmarinvaldes <amvaldes010117@gmail.com>
2023-04-30 20:50:32 +0200

    Se agrega el proyecto

---
+++ funtions/Class.py
@@ -0,0 +1,231 @@
+import json
+import pymongo
+import requests
+import random
+import re
+import os
+from tkinter import *
+import subprocess as sub
+import pyttsx3
+import pywhatkit
+import speech_recognition as sr
+from tabulate import tabulate
+from pymongo import MongoClient
+
+
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+engine.setProperty('rate', 145)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def conexion():
+    client = pymongo.MongoClient("mongodb://localhost:27017")
+    return client
+
+class Cancion:
+    def __init__(self, nombre, cantante, genero, album, url):
+        self.nombre = nombre
+        self.cantante = cantante
+        self.genero = genero
+        self.album = album
+        self.url = url
+
+    def agregar_canciones_bd(url):
+        # Conexión a la base de datos
+        client = conexion()
+        db = client.MusicPlayList
+
+        # Obtener datos de la URL
+        response = requests.get(url)
+        canciones_json = json.loads(response.text)['results']
+
+        # Insertar cada canción en la base de datos
+        for cancion_json in canciones_json:
+            cancion = Cancion(
+                cancion_json['trackName'],
+                cancion_json['artistName'],
+                cancion_json['primaryGenreName'],
+                cancion_json['collectionName'],
+                cancion_json['trackViewUrl']
+            )
+            db.canciones.insert_one(cancion.__dict__)
+
+        print("Canciones insertadas en la base de datos.")
+
+def extructurarData_Base():
+    client = conexion()
+    db = client.MusicPlayList
+    lc = db.list_collection_names()
+    if "usuario" not in lc:
+        db.create_collection("usuario")
+        talk("La Base de Datos MusicPlayList ha creado exitosamente")
+        talk("Colección usuario se ha creada correctamente")
+    else:
+        talk("La colección usuario ya existe")
+    if "playlist" not in lc:
+        db.create_collection("playlist")
+        talk("Colección playlist creada correctamente")
+    else:
+        talk("La colección playlist ya existe")
+    if "canciones" not in lc:
+        db.create_collection("canciones")
+        talk("Colección canciones creada correctamente")
+    else:
+        talk("La colección canciones ya existe")
+    
+
+# def validacion_email(correo):
+#     re = r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"
+#     return re.match(re, correo) is not None
+
+
+def validacion_user():
+    global namecontact_entry
+    window_user = Toplevel()
+    window_user.title("I-USER")
+    window_user.geometry("300x350")
+    window_user.resizable(0, 0)
+    window_user.configure(background="#666F80")
+
+    lista = []
+    nombre = []
+    apellido = []
+    usuario = []
+    email = []
+
+    def guardar():
+        nonlocal nombre, apellido, usuario, email
+        if nombre is str():
+            n = nombre.capitalize()
+            lista.append(n)
+        else:
+            talk("Por favor ingrese un nombre válido (solo letras).")
+            return
+
+        if apellido is str():
+            a = apellido.capitalize()
+            lista.append(a)
+        else:
+            talk("Por favor ingrese un apellido válido (solo letras).")
+            return
+
+        if usuario is str():
+            lista.append(usuario)
+        else:
+            talk("Por favor ingrese un nombre de usuario válido (sin signos de puntuación).")
+            return
+
+        if email is str():
+            lista.append(email)
+        else:
+            talk("Por favor ingrese un correo electrónico válido.")
+            return
+
+        window_user.destroy()
+
+    title_label = Label(window_user, text="Incert Nombre",
+                        fg="White", bg="#666F80", font=('Arial', 10, 'bold'))
+    title_label.pack(pady=3)
+
+    nombre_entry = Entry(window_user)
+    nombre.append(nombre_entry)
+    nombre_entry.pack(pady=1)
+    nombre_entry.focus()
+    talk("Por favor introduzca un nombre válido (Solo Letras)")
+
+    title_label = Label(window_user, text="Incert Apellido",
+                        fg="White", bg="#666F80", font=('Arial', 10, 'bold'))
+    title_label.pack(pady=3)
+
+    apellido_entry = Entry(window_user)
+    apellido.append(apellido_entry)
+    apellido_entry.pack(pady=1)
+    talk("Por favor introduzca un apellido válido (Solo Letras)")
+
+    title_label = Label(window_user, text="Incert Usuario",
+                        fg="White", bg="#666F80", font=('Arial', 10, 'bold'))
+    title_label.pack(pady=3)
+
+    usuario_entry = Entry(window_user)
+    usuario.append(usuario_entry)
+    usuario_entry.pack(pady=1)
+    talk("Por favor usuario válido (Sin signos de puntuación)")
+
+    title_label = Label(window_user, text="Incert email",
+                        fg="White", bg="#666F80", font=('Arial', 10, 'bold'))
+    title_label.pack(pady=3)
+
+    email_entry = Entry(window_user)
+    email.append(email_entry)
+    email_entry.pack(pady=1)
+    talk("Por favor introduzca un correo válido")
+
+    save_button = Button(window_user, text="Guardar", bg='#FF9EA0',
+                         fg="black", width=7, height=1, command=guardar)
+    save_button.pack(pady=4)
+
+    window_user.bind('<Return>', lambda event: guardar())
+
+    # def valida_user_name(username):
+    #     # validar que el nombre de usuario no contiene espacios
+    #     if " " in username:
+    #         talk("El nombre de usuario no puede contener espacios.")
+    #         return ""
+    #     return username
+
+    # def validacion_email(email):
+    #     # validar que el correo contiene un "@" y un "."
+    #     if "@" not in email and "." not in email:
+    #         talk("El correo electrónico no es válido.")
+    #         return False
+    #     return True
+
+
+    # def validacion_email(email):
+    #     # validar que el correo tenga formato válido usando una expresión regular
+    #     regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    #     if not re.match(regex, email):
+    #         talk("El correo electrónico no es válido.")
+    #         return False
+    #     return True
+    def procesar_usuario():
+        datos_usuario = lista
+        if len(datos_usuario) == 4:
+            nombre = datos_usuario[0]
+            apellido = datos_usuario[1]
+            usuario = datos_usuario[2]
+            email = datos_usuario[3]
+            crear_user(nombre, apellido, usuario, email)
+        else:
+            talk("Error en los datos del usuario")
+
+def crear_user(nombre, apellido, usuario, email):
+    client = conexion()
+    db = client.MusicPlayList
+    cursor = db.usuario.find_one({"username":usuario})
+    if cursor is None:
+        user = ([{
+            "nombre":nombre,
+            "apellido":apellido,
+            "username":usuario,
+            "email":email
+        }])
+        db.usuario.insert_many(user)
+        talk("Usuario creado con éxito")
+        # Mensaje de confirmación
+        message = f"Se ha creado la cuenta para {usuario} con éxito."
+        messagebox.showinfo("Registro exitoso", message)
+    else:
+        talk(f"Ya existe un usuario ({usuario}) ")
+        talk("Por favor inténte con otro username")
+
+    window_user.mainloop()
+
+
+
+
---
+++ princiapl.py
@@ -0,0 +1,131 @@
+from tkinter import *
+import subprocess as sub
+import pyttsx3
+import pywhatkit
+import speech_recognition as sr
+import json
+import requests
+import pymongo
+from pymongo import MongoClient
+from funtions import Class as cl
+
+
+# Crear ventana principal
+main_window = Tk()
+main_window.title("GUI")
+main_window.geometry("500x400")
+main_window.resizable(0, 0)
+main_window.configure(bg='#FF9EA0')
+
+Label_title = Label(main_window, text="Proyecto 2 por Ariel Marín", bg="#FF9EA0",
+                    fg="black", font=('Arial', 12, 'bold'))
+Label_title.pack(pady=5)
+
+cm = """
+Por favor siga los pasos
+
+1- crea la estructura de la DB
+2- Incerte las canciones de los artista
+3- cree un usuario
+4- mostrar sugerencias
+5- finalizar el programa
+"""
+canvas_comandos = Canvas(bg="#666F80", height=100, width=200)
+canvas_comandos.place(x=1, y=50)
+canvas_comandos.create_text(90, 50, fill="#FFFFFF", text=cm, font='Arial 5')
+
+canvas_dos = Canvas(bg="#C3C8D3", height=100, width=200)
+canvas_dos.place(x=295, y=50)
+canvas_dos.create_text(90, 50, fill="#434343", font='Arial 5')
+
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+engine.setProperty('rate', 145)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def termina():
+    talk("Fin del programa")
+    main_window.destroy()
+
+
+def abrir_ventana_agregar_artista():
+    # Crear ventana emergente para agregar artista
+    ventana_agregar_artista = Toplevel()
+    ventana_agregar_artista.title("Agregar Artista")
+    ventana_agregar_artista.geometry("300x150")
+    ventana_agregar_artista.configure(background="#666F80")
+    ventana_agregar_artista.resizable(0, 0)
+    main_window.eval(f'tk::PlaceWindow {str(ventana_agregar_artista)} center')
+
+
+    # Crear etiqueta y entrada para ingresar el nombre del artista
+    name_label = Label(ventana_agregar_artista, text="Nombre del Artista", fg="White",
+                       bg="#666F80", font=('Arial', 7, 'bold'))
+    name_label.pack(pady=5)
+
+    Label(ventana_agregar_artista, text="Incert User", fg="White", bg="#666F80", font=('Arial', 10, 'bold'))
+    nombre_artista_entry = Entry(ventana_agregar_artista)
+    nombre_artista_entry.pack(pady=1)
+    # Función para actualizar la URL principal de la solicitud de datos
+    def actualizar_url_principal():
+        try:
+            nombre_artista = nombre_artista_entry.get()
+            url_principal = f"https://itunes.apple.com/search?term={nombre_artista}&limit=5"
+            cl.Cancion.agregar_canciones_bd(url_principal)
+            ventana_agregar_artista.destroy()
+        except KeyError:
+            talk("Error    No se pudo agregar ese artista")
+
+    # Crear botón para actualizar la URL principal y agregar las canciones a la base de datos
+    save_button = Button(ventana_agregar_artista, text="Guardar", bg='#FF9EA0',
+                        fg="black", width=7, height=1, command=actualizar_url_principal)
+    save_button.pack(pady=4)
+
+
+# def incertar_user():
+#     global namecontact_entry
+#     window_user = Toplevel()
+#     window_user.title("I-USER")
+#     window_user.geometry("300x200")
+#     window_user.resizable(0, 0)
+#     window_user.configure(background="#666F80")
+#     main_window.eval(f'tk::PlaceWindow {str(window_user)} center')
+
+#     title_label = Label(window_user, text="Incert User",
+#                         fg="White", bg="#666F80", font=('Arial', 10, 'bold'))
+#     title_label.pack(pady=3)
+#     name_label = Label(window_user, text="User name", fg="White",
+#                        bg="#666F80", font=('Arial', 7, 'bold'))
+#     name_label.pack(pady=2)
+
+#     namecontact_entry = Entry(window_user)
+#     namecontact_entry.pack(pady=1)
+
+#     save_button = Button(window_user, text="Guardar", bg='#FF9EA0',
+#                          fg="black", width=7, height=1)
+#     save_button.pack(pady=4)
+
+# Botones para efectuar los pasos para crear y ehjecutar la base de datos
+
+button_paso_uno = Button(main_window, text="1", command=cl.extructurarData_Base)
+button_paso_uno.place(x=10, y=170)
+
+button_paso_dos = Button(main_window, text="2", command=abrir_ventana_agregar_artista)
+button_paso_dos.place(x=50, y=170)
+
+button_paso_tres = Button(main_window, text="3", command=cl.validacion_user)
+button_paso_tres.place(x=90, y=170)
+
+button_paso_cuatro = Button(main_window, text="4")
+button_paso_cuatro.place(x=130, y=170)
+
+button_paso_cinco = Button(main_window, text="5", command=termina)
+button_paso_cinco.place(x=170, y=170)
+
+main_window.mainloop()
